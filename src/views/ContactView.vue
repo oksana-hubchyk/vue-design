b78895ec0fafdeb780b2f5a2bdf14c2c331594e5
Oksana Hubchyk <oksana.hubchyk@example.com>
2025-11-23 23:03:41 +0200

    Initial commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import UserCard from '@/components/UserCard.vue';
+</script>
+
+<template>
+  <div class="contact-page">
+    <h1>Сторінка контактів</h1>
+    <p>Це наша нова сторінка контактів!</p>
+
+    <UserCard username="Student_1" :isActive="true" />
+    <UserCard username="Student_2" :isActive="1" /> <!--Додана помилка - число "1" замість значення "false"-->
+
+  </div>
+</template>
+
+<style scoped>
+.contact-page {
+  padding: 20px;
+}
+</style>
